cff88b811bbb7dc574ab1132e73b266dcb4c7f8f
muntazir ahmed <muntazirahmed7@gmail.com>
2022-02-26 14:08:14 +0530

    discount cal
    calculated discount and amount to be paid after discount using a function;

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var billamount=document.querySelector("#bill-amount");
 var discount=document.querySelector("#discount");
 var discountamount=document.querySelector("#discount-amnt");
-var amount=document.querySelector("amount-paid");
+var amount=document.querySelector("#amount-paid");
 var btn=document.querySelector("#button");
 var output=document.querySelector("#output");
 
@@ -9,7 +9,24 @@ var output=document.querySelector("#output");
 
 
 function calculate(){
+   output.innerText=""
 
-   var 
+   if((billamount.value !=="") && (discount.value !=="")){
+     let dis=discount.value/100*(billamount.value);
+     discountamount.value=dis;
+     let amounttopay=billamount.value-discountamount.value;
+     amount.value=amounttopay;
+   }
+   else{
+      if(billamount.value===""){
+      output.innerText="enter valid  billamount";
+   }
+   else if (discount.value==="")
+   {
+      output.innerText="enter valid  discount";
+
+   }
    
-}
+}}
+
+   
