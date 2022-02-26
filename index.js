e39b3dd37f90902b885ea37d263dac016687e074
muntazir ahmed <muntazirahmed7@gmail.com>
2022-02-26 13:24:14 +0530

    eventlistener to claculate discount

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
-var inputs=document.querySelectorAll('.input');
+var billamount=document.querySelector("#bill-amount");
+var discount=document.querySelector("#discount");
+var discountamount=document.querySelector("#discount-amnt");
+var amount=document.querySelector("amount-paid");
 var btn=document.querySelector("#button");
 var output=document.querySelector("#output");
 
+ btn.addEventListener('click',calculate)
 
 
+function calculate(){
+
+   var 
+   
+}
